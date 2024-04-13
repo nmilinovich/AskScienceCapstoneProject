800d617625bfb7b6c07171765ef940ca81687a1d
Nikola Milinovich <nmilinovich1@gmail.com>
2024-04-12 18:38:31 -0700

    changed seeds

--- backend/db/seeders/20240413001246-demo-image.js
+++ backend/db/seeders/20240413001246-demo-image.js
@@ -8,31 +8,30 @@ module.exports = {
     await Image.bulkCreate([
       {
         userId: 1,
-        likeableType: 'question',
-        likeableId: 1,
+        imageableType: 'question',
+        imageableId: 1,
         url: 'url 1'
       },
       {
         userId: 2,
-        likeableType: 'question',
-        likeableId: 2,
+        imageableType: 'question',
+        imageableId: 2,
         url: 'url 2'
       },
       {
         userId: 1,
-        likeableType: 'answer',
-        likeableId: 1,
+        imageableType: 'answer',
+        imageableId: 1,
         url: 'url 3'
       },
     ], { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'Images';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      imageableType: { [Op.in]: ['question', 'answer'] }
+    }, {});
   }
 };
