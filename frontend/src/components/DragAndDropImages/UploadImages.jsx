241f172ec3b49e45c5379f9ea86edf641094c218
Nikola Milinovich <nmilinovich1@gmail.com>
2024-04-20 18:39:26 -0700

    user can now upload images with their answer

--- frontend/src/components/DragAndDropImages/UploadImages.jsx
+++ frontend/src/components/DragAndDropImages/UploadImages.jsx
@@ -1,33 +1,11 @@
-import { useState } from "react";
-import './UploadImages'
-function UploadImages() {
-  const [selectedImages, setSelectedImages] = useState([]);
-  return (
-    <div className="imageUploadContainer">
-      {selectedImages.map(img => (
-        <div>
-          <img
-            alt="not found"
-            width={"250px"}
-            src={URL.createObjectURL(img)}
-          />
-          <br />
-          {console.log(selectedImages)}
-          <button onClick={() => setSelectedImages(selectedImages.filter((keptImgs) => keptImgs !== img))}>Remove</button>
-        </div>
-      ))}
-      <input
-        type="file"
-        name="myImage"
-        accept="image/*"
-        onChange={(event) => {
-          console.log(event.target.files[0]);
-          setSelectedImages(selectedImages => [...selectedImages, ...event.target.files]);
-        }}
-      />
-    </div>
-  );
-};
+// import { useState } from "react";
+// import './UploadImages'
+// function UploadImages() {
+
+//   return (
+
+//   );
+// };
 
 
-export default UploadImages
+// export default UploadImages
--- frontend/src/components/Questions/PostAnswer/PostAnswer.jsx
+++ frontend/src/components/Questions/PostAnswer/PostAnswer.jsx
@@ -1,17 +1,32 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import UploadImages from '../../DragAndDropImages/UploadImages';
+import { postNewAnswer } from '../../../store/answers';
+import { postNewImages } from '../../../store/images';
+import { getQuestionDetails } from '../../../store/questions';
+// import UploadImages from '../../DragAndDropImages/UploadImages';
 import './PostAnswer.css'
 
+function convertImageToBase64(file) {
+    const reader = new FileReader();
+    return new Promise(res => {
+        reader.onload = () => {
+            res(reader.result);
+        };
+        reader.readAsDataURL(file);
+    });
+};
+
 function PostAnswer() {
+    let imageableType='answer'
     const dispatch = useDispatch();
     let { questionId } = useParams();
     questionId = parseInt(questionId)
     const [description, setDescription] = useState('');
-    const [urlImages, setUrlImages] = useState([...Array(5)].map(_ => ''));
+    const [selectedImages, setSelectedImages] = useState([]);
     const [errors, setErrors] = useState({})
-    const onSubmit = e => {
+    const onSubmit = async (e) => {
+        console.log(selectedImages)
         e.preventDefault();
         setErrors({});
         const newAnswer = {
@@ -22,26 +37,27 @@ function PostAnswer() {
             errHits.questionId = "Related Question could not be found";
         }
         let errHits = {}
-        if (!urlImages[0]) {
-            errHits.urlImages = "You must provide a preview image for your spot.";
-        }
-        if (description.length < 30) {
-            errHits.description = "Description must be more than 30 characters.";
-        }
+        // if (description.length < 30) {
+        //     errHits.description = "Description must be more than 30 characters.";
+        // }
         setErrors(errHits);
-        if (!Object.values(errHits).length) {
-            // return dispatch(postNewSpot(newAnswer, urlImages.filter((url) => url)))
-            // .catch(async (res) => {
-            //     const data = await res.json();
-            //     console.log(errors);
-            //     console.log(errHits)
-            //     return setErrors(errors)
-            // });
+        if (!Object.values(errors).length) {
+            const answer = await new Promise(res => dispatch(postNewAnswer(newAnswer)).then(res));
+            if (selectedImages.length) {
+                let imageableId = answer.id
+                const base64Images = await Promise.all(selectedImages.map(convertImageToBase64));
+                console.log(base64Images);
+                const newImages = await new Promise(res => dispatch(postNewImages(base64Images, imageableType, imageableId)).then(res));
+            }
+            if (answer.ok) {
+                await new Promise(res => dispatch(getQuestionDetails()).then(res));
+            }
         }
     };
 
     return (
         <div >
+
             <form onSubmit={onSubmit} className='postAnswerForm'>
             <h3 className='responseH3'>Your Response</h3>
                 <label htmlFor='description'>
@@ -54,9 +70,30 @@ function PostAnswer() {
                     >
                     </textarea>
                 </label>
-                <UploadImages />
-                {console.log(UploadImages.selectedImages)}
-                <button onSubmit={onSubmit}>Submit Answer</button>
+                <div className="imageUploadContainer">
+      {selectedImages.map(img => (
+        <div key={img}>
+          <img
+            alt="not found"
+            width={"250px"}
+            src={URL.createObjectURL(img)}
+          />
+          <br />
+          {console.log(selectedImages)}
+          <button onClick={() => setSelectedImages(selectedImages.filter((keptImgs) => keptImgs !== img))}>Remove</button>
+        </div>
+      ))}
+      <input
+        type="file"
+        name="myImage"
+        accept="image/*"
+        onChange={(event) => {
+          console.log(event.target.files[0]);
+          setSelectedImages(selectedImages => [...selectedImages, ...event.target.files]);
+        }}
+      />
+    </div>
+                <button disabled={!description} onSubmit={onSubmit}>Submit Answer</button>
             </form>
         </div>
   );
