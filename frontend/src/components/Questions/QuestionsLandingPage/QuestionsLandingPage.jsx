c8f6c12ae2758d0d3101c0fa05697e6ddc35ed5d
Nikola Milinovich <nmilinovich1@gmail.com>
2024-11-09 14:30:09 -0800

    added search

--- frontend/src/components/Questions/QuestionsLandingPage/QuestionsLandingPage.jsx
+++ frontend/src/components/Questions/QuestionsLandingPage/QuestionsLandingPage.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getQuestions } from '../../../store/questions';
 import { useDispatch, useSelector } from "react-redux";
 import QuestionTile from '../QuestionTile/QuestionTile';
 
 function QuestionsLandingPage() {
+    const [search, setSearch] = useState('');
+
     const dispatch = useDispatch();
     const questions = useSelector((state) => state.questions)
     useEffect(() => {
@@ -12,10 +14,17 @@ function QuestionsLandingPage() {
     if (!questions) {
         return <div>Loading...</div>
     }
+
     return (
         <div className='landingPageDiv'>
+            <input
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+            />
             <div className='questions-grid-container'>
-                {Object.values(questions)?.map((question) => {
+                {Object.values(questions)
+                .filter((q) => !search || (q.title + q.description).toLowerCase().includes(search.toLowerCase()))
+                ?.map((question) => {
                     return (
                         <QuestionTile key={question.id} question={question}/>
                     )
